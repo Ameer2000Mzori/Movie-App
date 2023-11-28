c5b1efcd23e6c81a22330a2b5969911327c918fd
Ameer <ameerapex496@gmail.com>
2023-11-28 20:28:38 +0100

    added some text to the ts file

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,11 @@
-console.log("hallo world");
+// selecting our elements
+
+// getting our api keys
+// search key : https://api.themoviedb.org/3/search/movie?api_key=8dbad61c621c6aded1acf7750c8edc2a&query=A
+// normal Key : https://api.themoviedb.org/3/movie/popular?api_key=8dbad61c621c6aded1acf7750c8edc2a
+
+// api fetch function
+
+// dynamclly adding data
+
+// our event lisnters
